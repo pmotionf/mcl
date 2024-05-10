46f6364388469bc3772f54d2b4271c00f5777739
mochalins <117967760+mochalins@users.noreply.github.com>
2024-05-10 10:31:35 +0900

    scm: Update build for latest Zig master

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const version = b.addModule("version", .{
-        .root_source_file = .{ .path = "version.zig" },
+        .root_source_file = b.path("version.zig"),
     });
 
     const mdfunc_lib_path = b.option(
@@ -13,9 +13,9 @@ pub fn build(b: *std.Build) !void {
         "mdfunc",
         "Specify the path to the MELSEC static library artifact.",
     ) orelse if (target.result.cpu.arch == .x86_64)
-        b.pathFromRoot("vendor/mdfunc/lib/x64/MdFunc32.lib")
+        "vendor/mdfunc/lib/x64/MdFunc32.lib"
     else
-        b.pathFromRoot("vendor/mdfunc/lib/mdfunc32.lib");
+        "vendor/mdfunc/lib/mdfunc32.lib";
 
     const mdfunc = b.dependency("mdfunc", .{
         .target = target,
@@ -24,7 +24,7 @@ pub fn build(b: *std.Build) !void {
     });
 
     _ = b.addModule("mcl", .{
-        .root_source_file = .{ .path = "src/mcl.zig" },
+        .root_source_file = b.path("src/mcl.zig"),
         .imports = &.{
             .{ .name = "version", .module = version },
             .{ .name = "mdfunc", .module = mdfunc.module("mdfunc") },
