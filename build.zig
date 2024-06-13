e95d7fa0e8317dc2812ac076cbfbed0aa835a788
mochalins <117967760+mochalins@users.noreply.github.com>
2024-06-13 15:20:38 +0900

    scm: Add option to build mock `mdfunc.zig`

--- build.zig
+++ build.zig
@@ -17,10 +17,17 @@ pub fn build(b: *std.Build) !void {
     else
         "vendor/mdfunc/lib/mdfunc32.lib";
 
+    const mdfunc_mock_build = b.option(
+        bool,
+        "mdfunc_mock",
+        "Enable building a mock version of the MELSEC data link library.",
+    ) orelse (target.result.os.tag != .windows);
+
     const mdfunc = b.dependency("mdfunc", .{
         .target = target,
         .optimize = optimize,
         .mdfunc = mdfunc_lib_path,
+        .mock = mdfunc_mock_build,
     });
 
     _ = b.addModule("mcl", .{
